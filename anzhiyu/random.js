81b274183eff5a5e8de7617d159f8b774060ca47
liaries3 <1729709590@qq.com>
2025-06-28 12:32:23 +0800

    更新内容: YYYY-06-2025年6月28日 12:32:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/27/xingce1/"];function toRandomPost(){
+var posts=["2025/06/28/anbujiuban/","2025/06/27/xingce1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
